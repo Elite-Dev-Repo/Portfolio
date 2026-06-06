be216f1c3f52f92f01cabb44f9b9f50ad875261d
Elite Dev <oyenekantomiwa2.com>
2026-06-06 17:20:19 +0100

    cookie added

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-});
+  build: {
+    rollupOptions: {
+      // Treats "cookie" as an external dependency so Rollup doesn't crash trying to bundle it
+      external: ['cookie'],
+    },
+  },
+})
